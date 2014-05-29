4586aa9fe0000d72ab000183c77713c976de3734
yongyuanstu@gmail.com <hub884151git>
2014-05-29 20:14:00 +0800

    inite

---
+++ cocabulary.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import pickle
+from PCV.imagesearch import vocabulary
+from PCV.tools.imtools import get_imlist
+from PCV.localdescriptors import sift
+
+imlist = get_imlist('./first500/')
+nbr_images = len(imlist)
+featlist = [imlist[i][:-3]+'sift' for i in range(nbr_images)]
+
+#for i in range(nbr_images):
+#    sift.process_image(imlist[i], featlist[i])
+
+voc = vocabulary.Vocabulary('ukbench')
+voc.train(featlist, 1000, 10)
+# saving vocabulary
+with open('./first500/vocabulary.pkl', 'wb') as f:
+    pickle.dump(voc, f)
+print 'vocabulary is:', voc.name, voc.nbr_words
